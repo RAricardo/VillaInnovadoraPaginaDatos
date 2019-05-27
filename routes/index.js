f8f30707bb20ad872efcacc7a9b8d401c485eb29
Ricardo R. Azopardo <rrazopardc@eafit.edu.co>
2019-05-27 10:42:35 -0500

    make it better

--- routes/index.js
+++ routes/index.js
@@ -31,6 +31,7 @@ router.get("/jugadores", function (req, res) {
 
 router.get("/partidas/:id", function (req, res) {
     Partida.findById(req.params.id).populate("jugadores").exec(function (err, partida) {
+        console.log(partida);
         if (err) {
             console.log(err);
         } else {
